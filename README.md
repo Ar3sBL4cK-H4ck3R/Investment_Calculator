6166686cea7a7cef01010b39d24ee4758e54d2ce
Ar3s BL4cK <aqsaraheed860@gmail.com>
2025-08-19 05:27:49 +0300

    Clear some previous code comment values

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Investment_Calculator
-Investment Calculator with Go-Lang
+
+<b>Investment Calculator with Go-Lang </b>
+# Notes_Taking-Todo_App
+
+<b>Notes Taking and Todo App using Go-Lang</b>
 <br>
-This program is for anyone who want to create an Investment Calculator or want to use it in daliy life investments.
+<p>   Hey Guys! 
+This My new Project with Go-lang. In this Project I created a Notes Taking App for command line which is helpful when you want to create notes and store it in a json file. Also it save the data and time when you create this note. This program also contains the Todo APP which is also handy for anyone who want to create reminders. I made this app with my full potential and hard work. So don't forget to give me you feedback!!</p>
 <br>
-I hope You guys will enjoy it..
-<br>
-Don't Forget to give me Your FeedBack ;)
-<br>
-HAPPY CODING !!
+<h6>Importance:- </h6>
+    <p> This Program is For Anyone who want to code in GO or who want to just use it in daily hands on work.</p>
+<h6>Reminder:</h6>
+<p>
+    <b>Don't Forget to follow me on Instagram!</b>
+    <br>
+    If you have any query you cann ask me there :)
+<i>HAPPY CODING!!</i>
+</p>
--- insvestment_calculator.go
+++ insvestment_calculator.go
@@ -26,32 +26,10 @@ func main() {
 	fmt.Print("Enter Investment Horizon: ")
 	fmt.Scanln(&years)
 
-	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-
 	futureValue, futureRealValue := calculations(investmentAmount, expectedReturnRate, years)
 
 	output("Future Value $: %.2f\n", futureValue)
 	fmt.Printf("Future Real Value $: %.2f\n", futureRealValue)
-
-	// *We Can Also Store these string outputs in a variable as below:
-
-	// string_outputFV := fmt.Sprintf("Future Value $: %.2f\n", futureValue)
-	// string_outputFRV := fmt.Sprintf("Future Real Value $: %.2f\n", futureRealValue)
-
-	// fmt.Print(string_outputFV, string_outputFRV)
-
-	/* also we can use fmt.Printf() func to print all outputs in one fmt.printf() func
-	by separating values by commmas (,) */
-
-	// fmt.Printf("Future Value $: %.2f\nFuture Real Value $: %.2f\n", futureValue, futureRealValue)
-
-	// we can also use backticks to use multiple lines in a String without getting an error
-	//  if the string is much longer
-
-	/* fmt.Printf(`Future Value $: %.2f\n
-	Future Real Value $: %.2f\n`, futureValue, futureRealValue)*/
-
 }
 
 func output(value1 string, value2 float64) {
@@ -62,5 +40,4 @@ func calculations(investmentAmount, expectedReturnRate, years float64) (fv float
 	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	frv = fv / math.Pow(1+inflationRate/100, years)
 	return fv, frv
-	// return
 }
